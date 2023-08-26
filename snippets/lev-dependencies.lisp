83c2c77ac485ed4807a03a18f69d553396b607d9
Kris Cherven <krischerven@gmail.com>
2023-08-26 15:35:47 -0400

    snippets/lev-dependencies.lisp: Add missing function
    This is a line-by-line copy of alexandria:copy-array. Alexandria is in the public domain.

--- snippets/lev-dependencies.lisp
+++ snippets/lev-dependencies.lisp
@@ -1,6 +1,6 @@
 ;; Assorted dependencies required to compile the previous code snippet.
 ;; This file will only be of interest to you if you want to test the lev/lev* macro.
-;; Simply evaluate this file first, then lev.lisp second (only SBCL is supported.)
+;; Simply evaluate this file first, then lev.lisp second. Only SBCL is supported.
 ;; ---------------------------------------------------------------------------------
 ;;
 (eval-when (:compile-toplevel :load-toplevel :execute)
@@ -25,6 +25,23 @@
   (declare (ignore thing))
   t)
 
+(defun copy-array (array &key (element-type (array-element-type array))
+                              (fill-pointer (and (array-has-fill-pointer-p array)
+                                                 (fill-pointer array)))
+                              (adjustable (adjustable-array-p array)))
+  "Returns an undisplaced copy of ARRAY, with same fill-pointer and
+adjustability (if any) as the original, unless overridden by the keyword
+arguments."
+ (let* ((dimensions (array-dimensions array))
+        (new-array (make-array dimensions
+                               :element-type element-type
+                               :adjustable adjustable
+                               :fill-pointer fill-pointer)))
+   (dotimes (i (array-total-size array))
+     (setf (row-major-aref new-array i)
+           (row-major-aref array i)))
+   new-array))
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defstruct elist
     (:sequence nil :type (vector t))
@@ -44,7 +61,7 @@
   (fmakunbound 'copy-elist)
   (defun copy-elist (elist)
     (declare (type elist elist))
-    (make-elist :sequence (alexandria:copy-array (elist-sequence elist)) :typef (elist-typef elist))))
+    (make-elist :sequence (copy-array (elist-sequence elist)) :typef (elist-typef elist))))
 
 (defun extend (elist element)
   (declare (type elist elist))
